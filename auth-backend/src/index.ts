622fad35800a1b2c9ce08c3758ff0fd8c1625fd3
SteinerDiamine <shubhamsharma68663@gmail.com>
2025-03-14 10:57:54 +0530

    established redis connection

--- auth-backend/src/index.ts
+++ auth-backend/src/index.ts
@@ -1,6 +1,7 @@
 import app from './app';
 import { config } from 'dotenv';
 import { connectToDatabase } from './mysql/connection';
+import { intializeRedis } from './redis/connection';
 
 
 config();
@@ -9,6 +10,7 @@ const init  = async () => {
     //init mysql 
     try {
         await connectToDatabase();
+        await intializeRedis();
         const PORT = process.env.PORT || 5000;
         app.listen(PORT,  ()=> {
             console.log(`Server running on port,`, PORT);
