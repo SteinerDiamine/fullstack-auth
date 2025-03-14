fa1ea0b341b4f157efce0580e0de821adc4adecf
SteinerDiamine <shubhamsharma68663@gmail.com>
2025-03-14 07:56:13 +0530

    basic intialization

--- auth-backend/src/index.ts
+++ auth-backend/src/index.ts
@@ -1,1 +1,15 @@
-console.log("");
+import app from './app';
+import { config } from 'dotenv';
+
+
+config();
+
+const init  = async () => {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT,  ()=> {
+        console.log(`Server running on port,`, PORT);
+    }
+    );
+}
+
+init();
